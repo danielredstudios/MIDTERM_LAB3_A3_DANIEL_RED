4e5ee9fcc42766454b5ed57baac402328fdfc422
Daniel Rafael Red <danred206@gmail.com>
2025-09-10 13:40:00 +0800

    Add project files.

---
+++ MIDTERM_LAB3_DANIEL_RED/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MIDTERM_LAB3_DANIEL_RED.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
